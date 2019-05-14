b8b10b72435e06622b5d7e958f1908ac05257a6e
zmbslyr <mhedgeland@gmail.com>
2019-05-14 12:53:29 -0400

    Testing linked list

--- main.c
+++ main.c
@@ -12,7 +12,10 @@ int main(void)
 	head = malloc(sizeof(stack_t));
 	if (head == NULL)
 		return (-1);
-	head->n = 1;
+	head->n = 37;
+	head->prev = NULL;
+	head ->next = NULL;
 	printf("%d\n", head->n);
+	free(head);
 	return (0);
 }
